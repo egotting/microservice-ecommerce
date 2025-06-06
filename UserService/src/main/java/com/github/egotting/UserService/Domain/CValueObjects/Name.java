dc5a6c9924c71fc575a1ef90b2cb0f5e54fe6872
egotting <henricodss@gmail.com>
2025-06-06 10:36:29 -0300

    [Feat]: Implementation Name type

--- UserService/src/main/java/com/github/egotting/UserService/Domain/CValueObjects/Name.java
+++ UserService/src/main/java/com/github/egotting/UserService/Domain/CValueObjects/Name.java
@@ -1,4 +1,18 @@
 package com.github.egotting.UserService.Domain.CValueObjects;
 
 public class Name {
+    private String firstName;
+    private String lastName;
+
+    public Name(String firstName, String lastName) {
+        if (!isValid()) {
+            throw new IllegalArgumentException("First name and last name cannot be null or empty");
+        }
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public boolean isValid() {
+        return firstName != null && lastName != null && !firstName.toString().isEmpty() && !lastName.toString().isEmpty();
+    }
 }
