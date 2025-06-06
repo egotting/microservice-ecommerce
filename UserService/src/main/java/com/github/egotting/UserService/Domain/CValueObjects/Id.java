1c490b6f9567655feed057ffc80cc427626bc357
egotting <henricodss@gmail.com>
2025-06-06 10:29:22 -0300

    [Feat]: Implementation Id type

--- UserService/src/main/java/com/github/egotting/UserService/Domain/CValueObjects/Id.java
+++ UserService/src/main/java/com/github/egotting/UserService/Domain/CValueObjects/Id.java
@@ -1,5 +1,20 @@
 package com.github.egotting.UserService.Domain.CValueObjects;
 
-// TODO: Implement the Id using UUID
+import java.util.UUID;
+
 public class Id {
+    private UUID id;
+
+    public Id() {
+        if (isValid()) {
+            throw new IllegalArgumentException("Invalid ID");
+        }
+        this.id = UUID.randomUUID();
+    }
+
+
+    public boolean isValid() {
+        return this.id == null && id.toString().isEmpty();
+    }
+
 }
